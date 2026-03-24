eeffa1aafa601bd0bc04f9dee03722c8cb6ff4ec
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-03-25 02:24:05 +0530

    Day-2 Upgraded

--- Main.java
+++ Main.java
@@ -1,30 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
         //Modifying code
-        System.out.println("My name is Shivam");
-        System.out.println("I am learning Java Backend");
+        // System.out.println("My name is Shivam");
+        // System.out.println("I am learning Java Backend");
 
         //--- Day-1 --- Mini Task
-        System.out.println("Name: Shivam");
-        System.out.println("City: Varanasi");
-        System.out.println("Goal: Become a Java Developer");
+        // System.out.println("Name: Shivam");
+        // System.out.println("City: Varanasi");
+        // System.out.println("Goal: Become a Java Developer");
 
         /*-----------------------------------
         -----Day-2 Variables and Data Types--
         -------------------------------------*/
 
-        int age = 21;
-        double height = 5.9;
-        char grade = 'A';
-        String name = "Shivam";
-        boolean isStudent = true;
+        // int age = 21;
+        // double height = 5.9;
+        // char grade = 'A';
+        // String name = "Shivam";
+        // boolean isStudent = true;
 
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(height);
-        System.out.println(grade);
-        System.out.println(isStudent);
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(height);
+        // System.out.println(grade);
+        // System.out.println(isStudent);
+
+        /*-----Exercise-----*/
+        //using variables
+        // String name = "Shivam";
+        // int age = 26;
+        // String city = "Varanasi";
+
+        // System.out.println("My name is: " + name);
+        // System.out.println("I am " + age + " year old");
+        // System.out.println("I live in " + city);
+
+        /*-----Task-2-----*/
+        String productName = "Milk";
+        double price = 4.2;
+        boolean isAvailable = true;
+
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: $" + price);
+        System.out.println("In Stock: " + isAvailable);
     }
 }
