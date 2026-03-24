4e15330a8fd176f8e1fa820ad8780921afe88782
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-03-25 01:55:36 +0530

    Day-1

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        //Modifying code
+        System.out.println("My name is Shivam");
+        System.out.println("I am learning Java Backend");
+
+        //--- Day-1 --- Mini Task
+        System.out.println("Name: Shivam");
+        System.out.println("City: Varanasi");
+        System.out.println("Goal: Become a Java Developer");
+    }
+}
