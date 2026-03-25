c5add4872309d052fdee69f884b3551ab95e7cf7
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-03-25 21:29:37 +0530

    Day-4 Upgrade

--- Conditionals.java
+++ Conditionals.java
@@ -8,13 +8,42 @@ public class Conditionals {
         ---------------------------*/
 
         /*----------Exercise----------*/
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
 
-        if(num > 0) {
-            System.out.println("Positive");
+        // if(num > 0) {
+        //     System.out.println("Positive");
+        // } else {
+        //     System.out.println("Negative");
+        // }
+
+        // sc.close();
+
+        /*----------Exercise----------*/
+        // Scanner sc = new Scanner(System.in);
+        // int marks = sc.nextInt();
+
+        // if(marks >= 90) {
+        //     System.out.println("A");
+        // } else if(marks >= 75) {
+        //     System.out.println("B");
+        // } else if(marks >= 50) {
+        //     System.out.println("C");
+        // } else {
+        //     System.out.println("Fail");
+        // }
+
+        // sc.close();
+
+        /*-----------Task-4----------*/
+        //age eligibility checker
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Eligible");
         } else {
-            System.out.println("Negative");
+            System.out.println("Not Eligible");
         }
 
         sc.close();
