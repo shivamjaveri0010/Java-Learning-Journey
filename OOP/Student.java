4f41468cc44d4055090fd3da00c06ea16af3de0f
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-03-29 20:09:54 +0530

    Day-12 modified Student.java for Day-12 Exercise of Constructors

--- OOP/Student.java
+++ OOP/Student.java
@@ -4,22 +4,58 @@ package OOP;
 /*-----Classes & Objects-----*/
 /*----------Exercise----------*/
 
-public class Student {
+// public class Student {
 
+//     String name;
+//     int age;
+
+//     void displayDetails() {
+//         System.out.println("Name: " + name);
+//         System.out.println("Age: " + age);
+//     }
+
+//     public static void main(String[] args) {
+//         Student obj = new Student();
+
+//         obj.name = "Shivam";
+//         obj.age = 26;
+
+//         obj.displayDetails();
+//     }
+// }
+
+/*----------Day-12----------*/
+/*----------Exercise----------*/
+public class Student {
     String name;
     int age;
 
+    //Default Constructor
+    Student() {
+        System.out.println("Default Constructor Called");
+    }
+
+    //parameterized constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //method to print values
     void displayDetails() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
 
     public static void main(String[] args) {
-        Student obj = new Student();
 
-        obj.name = "Shivam";
-        obj.age = 26;
+        //creating object to call default constructor
+        Student defaultConstructor = new Student();
 
+        //calling parameterized constructor
+        Student obj = new Student("Shivam", 26);
+
+        //print values
         obj.displayDetails();
     }
 }
